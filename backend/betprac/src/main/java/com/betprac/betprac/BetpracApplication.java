6770e5196ec1a885373b8bc2405e6e773d834ab1
erikmarconato <erik.marconato@hotmail.com>
2025-03-05 23:56:25 -0300

    initial commit for backend - add springboot

---
+++ backend/betprac/src/main/java/com/betprac/betprac/BetpracApplication.java
@@ -0,0 +1,13 @@
+package com.betprac.betprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetpracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetpracApplication.class, args);
+	}
+
+}
---
+++ backend/betprac/src/test/java/com/betprac/betprac/BetpracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betprac.betprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetpracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
